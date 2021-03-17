f9d40312a1de89bfa98363af93fa649107101bc4
KazzModan <kklopov64@gmail.com>
2021-03-18 01:27:13 +0200

    addded Database and test insert

---
+++ DAL/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DAL
+{
+    public class MonopolyDbContext : System.Data.Entity.DbContext
+    {
+       
+        public MonopolyDbContext()
+            : base("name=MonopolyDb")
+        {
+            Database.SetInitializer(new Initializer());
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+
+    
+}
---
+++ DAL/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Index("IX_UniqueKeyString", IsUnique = true, Order = 1)]
+        [MaxLength(100)]
+        public string Login { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Password { get; set; }
+        [Required]
+        [MaxLength(150)]
+        [Index("IX_UniqueKeyString", IsUnique = true, Order = 2)]
+        public string Email { get; set; }
+        [DefaultValue(0)]
+        public int Rating { get; set; }
+        [DefaultValue(0)]
+        public int Money { get; set; }
+    }
+}
