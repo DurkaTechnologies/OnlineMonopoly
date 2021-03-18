d37e368211b0c4282b8175d146c5afd543920e00
ColdForeign <tankon46555@gmail.com>
2021-03-18 22:41:47 +0200

    Add sign in, up, game page and main menu page

---
+++ WPFUI/Pages/SignInPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using WPFUI.ViewModels;
+
+namespace WPFUI.Pages
+{
+	/// <summary>
+	/// Interaction logic for SignInPage.xaml
+	/// </summary>
+	public partial class SignInPage : Page
+	{
+		public SignInPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Page_Loaded(object sender, RoutedEventArgs e)
+		{
+			Navigation.Navigation.Service = NavigationService.GetNavigationService(this);
+			this.DataContext = new SignInViewModel();
+		}
+	}
+}
