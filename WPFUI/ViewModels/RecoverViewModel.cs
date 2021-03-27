f7d599f2af6197e675903a41f92f646e04549334
KazzModan <kklopov64@gmail.com>
2021-03-27 22:55:03 +0200

    refacroring

--- WPFUI/ViewModels/RecoverViewModel.cs
+++ WPFUI/ViewModels/RecoverViewModel.cs
@@ -22,17 +22,15 @@ namespace WPFUI.ViewModels
         private Command checkCommand;
         private static RecoverViewModel instance;
 
+        string server = "smtp.gmail.com"; // sets the server address
+        int port = 587; //sets the server port
+        private string key = "";
         public static RecoverViewModel GetInstance()
         {
             if (instance == null)
                 instance = new RecoverViewModel();
             return instance;
         }
-
-        string server = "smtp.gmail.com"; // sets the server address
-        int port = 587; //sets the server port
-        private string key = "";
-
         public string Mail
         {
             get => mail;
@@ -64,18 +62,12 @@ namespace WPFUI.ViewModels
         {
             Random random = new Random();
             for (int i = 0; i < 10; i++)
-            {
                 key += (Char)random.Next(33, 100);
-            }
             MailMessage message = new MailMessage("prodoq@gmail.com", mail, "recover Password", "Hello, u can recover your password, entered your key: "+key);
             message.Priority = MailPriority.High;
-            // create a send object
             SmtpClient client = new SmtpClient(server, port);
             client.EnableSsl = true;
-
-            // settings for sending mail
             client.Credentials = new NetworkCredential("prodoq@gmail.com", "r4e3w2q1");
-            // call asynchronous message sending
             client.SendAsync(message, "blabla");
             Navigation.Navigation.Navigate(Navigation.Navigation.SecondRecoverPageAlies, null);
             Text = "Enter key";
