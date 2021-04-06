c24cf91b2d2df4017b54a3c8ac8c656a1a004357
Oleksiy Maksumchyk <lykabraine27@gmail.com>
2021-04-06 18:46:34 +0300

    Offer User Control with inside logic was added

---
+++ WPFUI/UserControls/OfferControlAdditionals/OfferUser.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFUI.UserControls.OfferControlAdditionals
+{
+	/// <summary>
+	/// Interaction logic for OfferUser.xaml
+	/// </summary>
+	public partial class OfferUser : UserControl, INotifyPropertyChanged
+	{
+		#region Fields
+
+		private bool userType;
+		private string userName;
+		private string imageSource;
+		private ImageSource image;
+
+		#endregion
+
+		#region Proporties
+
+		public bool UserType 
+		{
+			get => userType;
+			set 
+			{
+				userType = value;
+				OnPropertyChanged();
+				OnPropertyChanged(nameof(UserTypeText));
+			}
+		}
+
+		public string UserName
+		{
+			get => userName;
+			set
+			{
+				userName = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public string UserTypeText => (UserType)? "Proposes" : "Gives";
+
+		public ImageSource Image 
+		{
+			get => image;
+			set 
+			{
+				image = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public string ImageSource
+		{
+			get => imageSource;
+			set
+			{
+				imageSource = value;
+				OnPropertyChanged();
+			}
+		}
+
+		#endregion
+
+		public OfferUser()
+		{
+			InitializeComponent();
+			InitializePropertyChanged();
+		}
+
+		#region INotify
+
+		private void InitializePropertyChanged()
+		{
+			PropertyChanged += (sender, args) =>
+			{
+				if (args.PropertyName.Equals(nameof(ImageSource)))
+				{
+					try
+					{
+						Image = new BitmapImage(new Uri(ImageSource));
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(e.Message);
+					}
+				}
+			};
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		#endregion
+	}
+}
