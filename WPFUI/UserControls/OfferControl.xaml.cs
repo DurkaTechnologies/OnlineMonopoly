b20d6baabe8be372756c9cef5530a9c5535d86e1
Oleksiy Maksumchyk <lykabraine27@gmail.com>
2021-04-06 23:10:56 +0300

    Offer User Money Control with inside logic was added

---
+++ WPFUI/UserControls/OfferControl.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFUI.UserControls
+{
+	/// <summary>
+	/// Interaction logic for OfferControl.xaml
+	/// </summary>
+	public partial class OfferControl : UserControl, INotifyPropertyChanged
+	{
+		private ICollection<object> userElements = new ObservableCollection<object>();
+
+		public IEnumerable<object> UserElements => userElements;
+
+		public OfferControl()
+		{
+			InitializeComponent();
+
+
+			for (int i = 0; i < 5; i++)
+			{
+				userElements.Add(new object());
+			}
+		}
+
+		#region INotify
+
+		private void InitializePropertyChanged()
+		{
+			PropertyChanged += (sender, args) =>
+			{
+				//if (args.PropertyName.Equals(nameof(ImageSource)))
+				//{
+				//	try
+				//	{
+				//		Image = new BitmapImage(new Uri(ImageSource));
+				//	}
+				//	catch (Exception e)
+				//	{
+				//		Console.WriteLine(e.Message);
+				//	}
+				//}
+			};
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		#endregion
+	}
+}
---
+++ WPFUI/UserControls/OfferControlAdditionals/OfferUsersMoney.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFUI.UserControls.OfferControlAdditionals
+{
+	/// <summary>
+	/// Interaction logic for OfferUsersMoney.xaml
+	/// </summary>
+	public partial class OfferUsersMoney : UserControl, INotifyPropertyChanged
+	{
+		private int userMoney;
+		private bool vis;
+
+		public int UserMoney 
+		{
+			get => userMoney;
+			set 
+			{
+				userMoney = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public bool Vis 
+		{
+			get => vis;
+			set 
+			{
+				vis = value;
+				OnPropertyChanged();
+				OnPropertyChanged(nameof(Hidden));
+			}
+		}
+
+		public string Hidden => (Vis) ? "Visible" : "Hidden";
+
+		public OfferUsersMoney()
+		{
+			InitializeComponent();
+			Vis = false;
+		}
+
+		private void RoundButton_Click(object sender, RoutedEventArgs e)
+		{
+			Vis = true;
+		}
+
+		private void CustomTextBox_MouseLeave(object sender, MouseEventArgs e)
+		{
+			Vis = false;
+		}
+
+		#region INotify
+
+		private void InitializePropertyChanged()
+		{
+			PropertyChanged += (sender, args) =>
+			{
+				//if (args.PropertyName.Equals(nameof(ImageSource)))
+				//{
+				//	try
+				//	{
+				//		Image = new BitmapImage(new Uri(ImageSource));
+				//	}
+				//	catch (Exception e)
+				//	{
+				//		Console.WriteLine(e.Message);
+				//	}
+				//}
+			};
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		#endregion
+
+		
+	}
+}
